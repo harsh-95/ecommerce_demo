07b56d11b44a81e261e7038046f57fd77b007304
Pegasus <bhanu1608@gmail.com>
2020-09-29 12:55:08 +0530

    mongodb connection setup

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const port = 8080;
+
+const connectionString = "mongodb+srv://demoUser:x2umTAa29blMQk0R@cluster0.2erxl.mongodb.net/testdb?retryWrites=true&w=majority";
+
+
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true }).then(
+    () => {
+        console.log('mongodb successfully connected');
+    }
+).catch(
+    (err) => {
+        console.log('unable to connect');
+        console.error(err);
+    }
+)
 
 app.get('/', (req, res) => {
     res.send("api response");
