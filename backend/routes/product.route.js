76913a4290f4cf637825cb93481f5ce6b19d9b74
AdityaPrasadP <48804953+AdityaPrasadP@users.noreply.github.com>
2020-10-05 16:58:29 +0530

    Updated get single product api and delete product api
    Updated get single product api and delete product api

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -7,4 +7,23 @@ module.exports = (app) => {
     app.post('/addProducts', productController.addProduct );
 
     app.put('/updateProduct', productController.updateProduct )
+
+    app.get('/:id',async(req,res)=>{
+        try{
+        const products= await Product.findById(req.params.id)
+        res.json(products)
+        }catch (err){
+            res.send("error" + err)
+        }
+
+    })
+
+    app.delete('/:postId',async(req,res)=>{
+        try{
+            const removedPost= await Product.remove({_id:req.params.postId})
+            res.json(removedPost)
+        }catch(err){
+            res.json({message:err})
+        }
+    })
 }
